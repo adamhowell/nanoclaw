185885bfad71038c0d00ba3e0e3d7a16a0842833
Adam Howell <adamhowell@gmail.com>
2026-03-28 19:44:22 -0400

    Debug: stringify message payload

--- src/channels/accomplice.ts
+++ src/channels/accomplice.ts
@@ -133,7 +133,10 @@ class AccompliceChannel implements Channel {
   }
 
   private handleMessage(msg: Record<string, unknown>): void {
-    logger.info({ msg }, 'Accomplice: handleMessage payload');
+    logger.info(
+      { msgKeys: Object.keys(msg), msgJson: JSON.stringify(msg) },
+      'Accomplice: handleMessage payload',
+    );
     switch (msg.type) {
       case 'user_message': {
         const jid = msg.conversation_jid as string;
